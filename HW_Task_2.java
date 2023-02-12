c3f70003189662c14942f5238468005e11f24f5e
Dizrtt <demonic2003@bk.ru>
2023-02-12 16:08:14 +0200

    Task № 3 completedd.

--- HW_Task_2.java
+++ HW_Task_2.java
@@ -2,7 +2,18 @@
 
 public class HW_Task_2 {
     public static void main(String[] args) {
-        System.out.print("\033[H\033[J");
 
+        System.out.print("\033[H\033[J");
+        boolean b = true;
+        for (int i = 2; i < 1001; i++) {
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0){
+                    b = false;
+                    break;
+                }
+            }
+            if (b == true) System.out.print(i + " ");
+            else b = true;
+        }
     }
 }
